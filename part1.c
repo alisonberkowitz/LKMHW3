27541d540f7817e9d86e5c37e6743c3dfa95877b
alisonberkowitz <alison.berkowitz@students.olin.edu>
2014-02-28 22:11:05 -0500

    started working on part 1

---
+++ part1.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mongo.h"
+
+int is_name(char *line) {
+  if (*line != '\t'){
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+void parse() {
+  FILE *in_file = fopen("dummy.txt", "r");
+  char line[100];
+  char *p;
+  char *name;
+  char *movie;
+  while(fgets(line,100,in_file)) {
+    if (is_name(line)){
+      p = strtok(line, "\t");
+      name = p;
+      printf("name: %s\n", name);
+      // add it to the db
+
+      p = strtok(NULL, "\t");
+      movie = p;
+      printf("movie: %s\n", movie );
+      // add it as child of name
+    }
+    else {
+      p = strtok(line, "\t");
+      movie = p;
+      printf("movie: %s\n", movie );
+      // add it as a child of name
+    }
+
+  }
+  fclose(in_file);
+}
+
+int main() {
+  mongo conn[1];
+  int status = mongo_client( conn, "127.0.0.1", 27017 );
+
+  if( status != MONGO_OK ) {
+      switch ( conn->err ) {
+        case MONGO_CONN_NO_SOCKET:  printf( "no socket\n" ); return 1;
+        case MONGO_CONN_FAIL:       printf( "connection failed\n" ); return 1;
+        case MONGO_CONN_NOT_MASTER: printf( "not master\n" ); return 1;
+      }
+  }
+
+  parse();
+
+  mongo_destroy( conn );
+
+  return 0;
+}
