5b4adf2d36585d5dfe7f4a86f21a935cd7042a08
alisonberkowitz <alison.berkowitz@students.olin.edu>
2014-03-04 13:42:53 -0500

    the seg fault is no longer because of the line length

--- part1.c
+++ part1.c
@@ -3,17 +3,17 @@
 #include <string.h>
 #include "mongo.h"
 
+#define MAX_NUMBER_CHILDREN 100
+#define LINE_LENGTH 1000
+
 typedef struct {
-  char *type;
   char *name;
-  char **children; //char **children
-  char path[20][80];
-  int pathLength;
+  char **children;
   int numberChildren;
 } Node;
 
 int is_name(char *line) {
-  if (*line != '\t'){
+  if ((*line != '\t') && (*line != '\n')){
     return 1;
   }
   else {
@@ -22,8 +22,8 @@ int is_name(char *line) {
 }
 
 void set_name(Node *n, char *guy) {
+  n->children = malloc(MAX_NUMBER_CHILDREN*sizeof(char*));
   n->numberChildren = 0;
-  n->type = "actor";
   n->name = (char*)malloc((strlen(guy)+1)*sizeof(char));
   strcpy(n->name, guy);
 }
@@ -35,9 +35,9 @@ void add_child(Node *n, char *movie) {
 }
 
 int totalActors(FILE *in_file) {
-  char line[100];
+  char line[LINE_LENGTH];
   int count = 0;
-  while(fgets(line,100,in_file)) {
+  while(fgets(line,LINE_LENGTH,in_file)) {
     if (is_name(line)){
       count = count + 1;
     }
@@ -45,47 +45,90 @@ int totalActors(FILE *in_file) {
   return count;
 }
 
-void parse( mongo *conn ) {
-  FILE *in_file = fopen("dummy.txt", "r");
-  char line[100];
+void parse( mongo *conn, FILE *in_file ) {
+  char line[LINE_LENGTH];
   char *p;
   char *guy;
   char *movie;
   int numberChildren;
   int total;
+  bson *a;
+  bson **as;
 
   total = totalActors(in_file);
-  printf("totalActors: %i\n", total);
+  //printf("totalActors: %i\n", total);
   rewind(in_file);
 
   Node *nodes = (Node*) malloc(sizeof(Node)*total);
   int g = -1;
 
-  while(fgets(line,100,in_file)) {
+  while(fgets(line,LINE_LENGTH,in_file)) {
     if (is_name(line)) {
       g = g+1;
       p = strtok(line, "\t");
       guy = p;
       set_name(&nodes[g], guy);
-      printf("name: %s\n", nodes[g].name);
+      //printf("name: %s\n", nodes[g].name);
 
       p = strtok(NULL, "\t");
       movie = p;
       add_child(&nodes[g], movie);
-      printf("movie: %s\n", nodes[g].children[0] );
+      //printf("movie: %s\n", nodes[g].children[0] );
+    }
+    else if (*line == '\n')
+    {
+      continue;
     }
     else {
       p = strtok(line, "\t");
       movie = p;
       add_child(&nodes[g], movie);
-      printf("name: %s\n, children:", nodes[g].name);
-      int i;
-      for (i=0; i<nodes[g].numberChildren; i++) {
-        printf("%s\n", nodes[g].children[i]);
-      }
+      // printf("name: %s\n, children:", nodes[g].name);
+      // int i;
+      // for (i=0; i<nodes[g].numberChildren; i++) {
+      //   printf("%s\n", nodes[g].children[i]);
+      // }
     }
   }
-  fclose(in_file);
+  int i, j;
+  as = ( bson ** )malloc( sizeof( bson * ) * total);
+  for ( i = 0; i < total; i++ ) {
+    a = ( bson * )malloc( sizeof( bson ) );
+    bson_init( a );
+
+    bson_append_new_oid( a, "_id" );
+    bson_append_string( a, "type", "actor" );
+    bson_append_string( a, "name", nodes[i].name );
+    bson_append_int( a, "numberChildren", nodes[i].numberChildren );
+    
+    bson_append_start_array(a, "children");
+    for (j = 0; j<(nodes[i].numberChildren); j++)
+    {
+      char str[15];
+      sprintf(str, "%d", j);
+      bson_append_string(a,str,nodes[i].children[j]);
+    }
+    bson_append_finish_array(a);
+
+    bson_finish( a );
+    as[i] = a;
+  }
+  mongo_insert_batch( conn, "prod.db", (const bson **) as, total, 0, 0 );
+
+  for ( i = 0; i < total; i++ ) {
+    bson_destroy( as[i] );
+    free( as[i] );
+  }
+}
+
+static void display_graph( mongo *conn) {
+  mongo_cursor cursor[1];
+  mongo_cursor_init( cursor, conn, "prod.db" );
+
+  while( mongo_cursor_next( cursor ) == MONGO_OK )
+    bson_print( &cursor->current );
+
+  mongo_cursor_destroy( cursor );
 }
 
 int main() {
@@ -100,8 +143,13 @@ int main() {
       }
   }
 
-  parse( conn );
+  mongo_cmd_drop_collection( conn, "prod", "db", NULL); 
 
+  FILE *in_file = fopen("actors.list", "r");
+  parse( conn, in_file );
+  fclose(in_file);
+
+  display_graph( conn );
   mongo_destroy( conn );
 
   return 0;
